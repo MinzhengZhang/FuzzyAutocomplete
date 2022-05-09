3493b83faebc58575be4ea091b59604e0248b121
MinzhengZhang <minzheng@seas.upenn.edu>
2022-05-09 18:36:22 -0400

    service layer test

---
+++ src/test/java/edu/upenn/cit594project/SearchServiceTest.java
@@ -0,0 +1,48 @@
+package edu.upenn.cit594project;
+
+import edu.upenn.cit594project.controller.SearchController;
+import edu.upenn.cit594project.service.ISearchService;
+import edu.upenn.cit594project.service.SearchService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = Cit594ProjectApplication.class,webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class SearchServiceTest {
+    @Autowired
+    private ISearchService service;
+
+
+
+    @Test
+    public void searchJaroWinklerTest(){
+        Collection collection = service.searchJaroWinkler("pikachu");
+        assertEquals(10,collection.size());
+    }
+
+    @Test
+    public void searchWeightedLevenshteinTest(){
+        Collection collection = service.searchWeightedLevenshtein("pikachu");
+        assertEquals(10,collection.size());
+    }
+
+    @Test
+    public void searchLevenshteinTest(){
+        Collection collection = service.searchLevenshtein("pikachu");
+        assertEquals(10,collection.size());
+
+    }
+
+    @Test
+    public void searchPhonetic(){
+        Collection collection = service.searchPhonetic("pikachu");
+        assertEquals(10,collection.size());
+    }
+
+
+}
